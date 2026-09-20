2bf6b7678135d6a50e10b82f275b865fc5838ecc
Eyasu Derbew Tegen <104543002+eyasuder@users.noreply.github.com>
2026-09-20 23:53:26 +0900

    Modify encoder pin definitions for Arduino Mega 2560
    Updated encoder pin definitions for Arduino Mega 2560.

--- ROSArduinoBridge/encoder_driver.h
+++ ROSArduinoBridge/encoder_driver.h
@@ -1,20 +1,19 @@
 /* *************************************************************
    Encoder driver function definitions - by James Nugen
    ************************************************************ */
-   
-   
+     
 #ifdef ARDUINO_ENC_COUNTER
-  //below can be changed, but should be PORTD pins; 
-  //otherwise additional changes in the code are required
-  #define LEFT_ENC_PIN_A PD2  //pin 2
-  #define LEFT_ENC_PIN_B PD3  //pin 3
+  //The encoder pins are connected arduino mega2650 AVR pin PORTK(which are
+  //analog pins A8 to A15)
+  //Analog left motor encoder
+  #define LEFT_ENC_PIN_A PK0  //pin A8
+  #define LEFT_ENC_PIN_B PK1  //pin A9 
   
-  //below can be changed, but should be PORTC pins
-  #define RIGHT_ENC_PIN_A PC4  //pin A4
-  #define RIGHT_ENC_PIN_B PC5   //pin A5
+  //Connect right motor encoder
+  #define RIGHT_ENC_PIN_A PK2  //pin A10
+  #define RIGHT_ENC_PIN_B PK3   //pin A11
 #endif
    
 long readEncoder(int i);
 void resetEncoder(int i);
 void resetEncoders();
-
