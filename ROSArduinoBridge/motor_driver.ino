4e7466b729cb74892b96d39923c36a1798a2252b
Eyasu Derbew Tegen <104543002+eyasuder@users.noreply.github.com>
2026-09-20 23:56:51 +0900

    Update motor_driver.ino

--- ROSArduinoBridge/motor_driver.ino
+++ ROSArduinoBridge/motor_driver.ino
@@ -57,11 +57,23 @@
   }
 #elif defined L298_MOTOR_DRIVER
   void initMotorController() {
-    digitalWrite(RIGHT_MOTOR_ENABLE, HIGH);
+
+    pinMode(RIGHT_MOTOR_ENABLE, OUTPUT);
+    pinMode(LEFT_MOTOR_ENABLE, OUTPUT);
+
+    pinMode(RIGHT_MOTOR_FORWARD, OUTPUT);
+    pinMode(RIGHT_MOTOR_BACKWARD, OUTPUT);
+
+    pinMode(LEFT_MOTOR_FORWARD, OUTPUT);
+    pinMode(LEFT_MOTOR_BACKWARD, OUTPUT);
+
+    digitalWrite(RIGHT_MOTOR_ENABLE,HIGH);
     digitalWrite(LEFT_MOTOR_ENABLE, HIGH);
+    
   }
-  
-  void setMotorSpeed(int i, int spd) {
+
+
+void setMotorSpeed(int i, int spd) {
     unsigned char reverse = 0;
   
     if (spd < 0)
@@ -73,19 +85,39 @@
       spd = 255;
     
     if (i == LEFT) { 
-      if      (reverse == 0) { analogWrite(LEFT_MOTOR_FORWARD, spd); analogWrite(LEFT_MOTOR_BACKWARD, 0); }
-      else if (reverse == 1) { analogWrite(LEFT_MOTOR_BACKWARD, spd); analogWrite(LEFT_MOTOR_FORWARD, 0); }
+      if (reverse == 0) { 
+        // FIXED: Forward pin gets PWM on positive speed
+        analogWrite(LEFT_MOTOR_FORWARD, spd);
+        analogWrite(LEFT_MOTOR_BACKWARD, 0); 
+      }
+        
+      else if (reverse == 1) {
+        // FIXED: Backward pin gets PWM on negative speed
+        analogWrite(LEFT_MOTOR_BACKWARD, spd); 
+        analogWrite(LEFT_MOTOR_FORWARD, 0); 
+      }
     }
-    else /*if (i == RIGHT) //no need for condition*/ {
-      if      (reverse == 0) { analogWrite(RIGHT_MOTOR_FORWARD, spd); analogWrite(RIGHT_MOTOR_BACKWARD, 0); }
-      else if (reverse == 1) { analogWrite(RIGHT_MOTOR_BACKWARD, spd); analogWrite(RIGHT_MOTOR_FORWARD, 0); }
+    else /*if (i == RIGHT)*/ {
+      if (reverse == 0) { 
+        // FIXED: Forward pin gets PWM on positive speed
+        analogWrite(RIGHT_MOTOR_FORWARD, spd);
+        analogWrite(RIGHT_MOTOR_BACKWARD, 0); 
+      }
+      else if (reverse == 1) { 
+        // FIXED: Backward pin gets PWM on negative speed
+        analogWrite(RIGHT_MOTOR_BACKWARD, spd);
+        analogWrite(RIGHT_MOTOR_FORWARD, 0); 
+      }
     }
   }
+
+
   
   void setMotorSpeeds(int leftSpeed, int rightSpeed) {
     setMotorSpeed(LEFT, leftSpeed);
     setMotorSpeed(RIGHT, rightSpeed);
-  }
+  } 
+ 
 #else
   #error A motor driver must be selected!
 #endif
